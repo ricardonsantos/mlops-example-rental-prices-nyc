8b14fd4b668529553df81e0c534e8e4c529f5a5b
Ricardo <rikchicfb@gmail.com>
2025-04-26 21:08:33 +0200

    added basic_cleaning and data_check steps, solved lib version issues

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""
+Download the raw dataset from W&B and apply basic data cleaning steps, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    logger.info("Perfoming data cleaning..")
+    run = wandb.init(group="basic_cleaning")
+
+    # retrieve the dataset artifact 
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info(f"Artifact successfully downloaded to {artifact_local_path}.\nLoading the data...")
+    df = pd.read_csv(artifact_local_path)
+
+    # Perform data cleansing
+    logger.info("Performing data cleansing..")
+    valid_prices = df["price"].between(args.min_price, args.max_price)
+    df_valid = df[valid_prices].copy() # remove outliers
+    df_valid["last_review"] = pd.to_datetime(df_valid["last_review"])
+
+    # Persist cleaned data
+    file_out = "clean_sample.csv"
+    df_valid.to_csv(file_out, index=False)
+    logger.info(f"Processed artifact saved locally ({file_out})")
+    artifact = wandb.Artifact(
+            args.output_artifact,
+            type=args.output_type, 
+            description=args.output_description
+            )
+    artifact.add_file(file_out)
+    run.log_artifact(artifact)
+    run.finish()
+    logger.info("Processed artifact saved remotely (W&B)")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="name of input artifact (raw database)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="name of output artifact (clean database)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="the type of the output file",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="a text describing the output file",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="the minimum valid price",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="the maximum valid price",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
