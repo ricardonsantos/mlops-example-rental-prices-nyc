d83fd48cef938c72e9ff88313c971d49fecb4423
Ricardo <rikchicfb@gmail.com>
2025-04-26 22:31:07 +0200

    data split and training WIP, fixed lib versions

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/get_data",
                 "main",
                 env_manager="conda",
-                version="develop",
+                version="main",
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
@@ -84,10 +84,18 @@ def go(config: DictConfig):
                 )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split", 
+                "main",
+                env_manager="conda",
+                version="main",
+                parameters={
+                    "input": "clean_sample.csv:latest", 
+                    "test_size": config["modeling"]["test_size"], 
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                    },
+                )
 
         if "train_random_forest" in active_steps:
 
